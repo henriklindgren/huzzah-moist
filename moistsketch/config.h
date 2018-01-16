795e9d35784f378fd4ce1fbd7c3cc8dbdb247ae3
henriklindgren <henriklindgren@users.noreply.github.com>
2018-01-16 21:47:11 +0100

    DHT11 support added.

--- moistsketch/config.h
+++ moistsketch/config.h
@@ -1,5 +1,8 @@
-//config.h
 #define SSID ""
 #define PASSWORD ""
 #define HOST ""
 #define ENDPOINT ""
+#define MOISTURESENSORANALOG A0
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define SLEEPTIMESECONDS 60
--- moistsketch/moistsketch.ino
+++ moistsketch/moistsketch.ino
@@ -6,42 +6,49 @@
  *  Hardware requirements:
  *    * Adafruit Feather HUZZAH ESP8266
  *    * Moisture sensor based on LM393
+ *    * Humidity and Temperature sensor DHT11 (or easily converted to using DHT22)
  *    
  *  Software requirements:
- *    * Programmed and uploaded using Arduino 1.6.9
+ *    * Programmed and uploaded using Arduino IDE 1.8.5
  *    See https://www.arduino.cc/en/Main/Software
- *    * Arduino ESP8266 plugin 2.3.0
- *    See http://esp8266.github.io/Arduino/versions/2.3.0/
+ *    * Arduino ESP8266 plugin 2.4.0
+ *    Using http://arduino.esp8266.com/stable/package_esp8266com_index.json
+ *    See https://github.com/esp8266/Arduino/releases/tag/2.4.0
+ *    
+ *    DHT library depends on Adafruit Sensor Library, both available by searching in the Arduino IDE.
  *    
  *    Code based on examples for ESP8266WiFi, Blink and AnalogInput.
  */
 
+#include <DHT.h>
+#include <DHT_U.h>
 #include <ESP8266WiFi.h>
-/*
- * Local configuration
- * 
- * SSID
- * PASSWORD
- * HOST
- * ENDPOINT
- */
+
 #include "config.h"
 
 WiFiClient client;
 const int httpPort = 80;
 
 //huzzah analog pin for the moisture sensor
-const int analogPin = A0;
+const int analogPin = MOISTURESENSORANALOG;
 // main red led on the huzzah
 // const int ledPin = 0;
+
 //expected to be between 0 and 1024
-int sensorValue = 0;
-const int secondsLoopDelay = 30;
+int soilMoisture = 0;
+int humidity = 0;
+int temperature = 0;
+DHT dht = DHT(DHTPIN, DHTTYPE);
+
+const int secondsLoopDelay = SLEEPTIMESECONDS;
 
 void setup() {  
   Serial.begin(115200);
-  delay(100);
-  Serial.print("Connecting to wifi SSID: ");
+  // Delay atleast a second before initializing DHT11 according to docs.
+  delay(1100);
+  dht.begin();
+  
+  Serial.print("Connecting to WiFi SSID: ");
   Serial.println(SSID);
   WiFi.begin(SSID, PASSWORD);
   
@@ -50,36 +57,41 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
+  Serial.println();
+  Serial.print("WiFi connected. IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  Serial.print("connecting to ");
+  Serial.print("Connecting to ");
   Serial.println(HOST);
   
   if (!client.connect(HOST, httpPort)) {
-    Serial.println("connection failed");
+    Serial.println("Connection failed.");
     return;
   } else {
-    // Make measurement
-    sensorValue = analogRead(analogPin);
+    // Make measurement.
+    soilMoisture = analogRead(analogPin);
     delay(100);
+
+    // Documentation states these measurements can take up to 250ms per measure.
+    humidity = (int)dht.readHumidity();
+    temperature = (int)dht.readTemperature();
+    Serial.println(String("moist:") + soilMoisture + " temp:" + temperature + " hum:" + humidity);
+    
     // Send measurement
-    client.print(String("GET ") + ENDPOINT + "?moisture=" + sensorValue + 
+    client.print(String("GET ") + ENDPOINT + "?moisture=" + soilMoisture + "&temp=" + soilMoisture + "&humidity=" + soilMoisture + 
       " HTTP/1.1\r\nHost: " + HOST + "\r\nConnection: close\r\n\r\n");
     delay(500);
     
-    // Read all the lines of the reply from server and print them to Serial
+    // Read all the lines of the reply from server and print them to Serial.
     while(client.available()){
       String line = client.readStringUntil('\r');
       Serial.print(line);
     }
     Serial.println();
      
-    Serial.println("closing connection");
+    Serial.println("Closing connection.");
     client.stop();
   } 
   delay(secondsLoopDelay * 1000);
